a057c71a8db8d0cd430cbeab8abc71cc7967fc46
ElizabethHub <bagiraliza21@gmail.com>
2024-03-24 00:13:09 +0200

    swagger water

--- schemas/waterSchemas.js
+++ schemas/waterSchemas.js
@@ -13,3 +13,15 @@ export const validateDate = Joi.object({
   year: Joi.number().integer().min(2000).max(2100).required(),
   month: Joi.number().integer().min(1).max(12).required(),
 });
+
+// for swagger
+// "requestBody": {
+//   "required": true,
+//   "content": {
+//     "application/json": {
+//       "schema": {
+//         "$ref": "#/components/schemas/WaterMonthReq"
+//       }
+//     }
+//   }
+// },
